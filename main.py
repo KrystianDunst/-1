1166182875a28222cd399d6b097c59acba2a5855
milochadam <miloch.adam@gmail.com>
2022-06-05 15:03:34 +0200

    Download Warszawska bus stops
    Download and then filter using requests

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from pprint import pprint
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    response = requests.get('http://api.zdiz.gdynia.pl/pt/stops')
+    # pprint(response.json())
+
+    filtered_stops = []
+    for stop in response.json():
+        if 'Warszawska' in stop['stopName']:
+            filtered_stops.append(stop)
+
+    pprint(filtered_stops)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
