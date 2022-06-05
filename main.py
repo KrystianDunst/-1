b22594121433f2dff358e20bad9973dcf4617523
milochadam <miloch.adam@gmail.com>
2022-06-05 15:38:20 +0200

    Print delay data

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ def main():
         if 'Warszawska' in stop['stopName']:
             filtered_stops.append(stop)
 
-    pprint(filtered_stops)
+    # pprint(filtered_stops)
+    delay_data = []
+    for stop in filtered_stops:
+        stop_id = stop['stopId']
+        data = requests.get(f'http://api.zdiz.gdynia.pl/pt/delays?stopId={stop_id}')
+        delay_data.extend(data.json()['delay'])
+
+    pprint(delay_data)
 
 
 if __name__ == '__main__':
